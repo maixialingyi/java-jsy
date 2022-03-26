de2fabaca5cd938b0bb3733c2f7c91dfe1f345ba
ext.jiangshaoyue1 <ext.jiangshaoyue1@jd.com>
2022-03-26 21:11:58 +0800

    1

--- zookeeper/src/main/java/com/jsy/learn/zk/Lock/TestLock.java
+++ zookeeper/src/main/java/com/jsy/learn/zk/Lock/TestLock.java
@@ -5,18 +5,13 @@ import org.junit.Test;
 public class TestLock {
     @Test
     public void lock() throws Exception {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             new Thread(() -> {
                 ZkLock zkLock = new ZkLock();
                 // 加锁
                 zkLock.lock("/appIdpayLock");
                 // 业务逻辑
                 System.out.println(Thread.currentThread().getName() + "执行业务逻辑");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 // 释放锁
                 zkLock.unLock();
             }).start();
